fd530080599ca2b89f40d912b1bbb18f19bac231
Jack Yeh <jack.yeh@gmail.com>
2015-03-26 20:31:06 -0400

    I4: added the guess limiting extension. Though it does have the hiccup of needing to have guessed 5 times before it will let you start a new game.

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,27 +2,37 @@ require 'sinatra'
 require 'sinatra/fix_951'
 require 'sinatra/reloader'
 
-NUMBER = rand(101)
+@@number = rand(101)
+@@counter = 5
 
-def resulting_message(your_guess)
-  if your_guess.to_i == NUMBER
-    "You got it right! The SECRET NUMBER is #{NUMBER}"
-  elsif your_guess.to_i > NUMBER + 5
-    "Way too high!"
-  elsif your_guess.to_i < NUMBER - 5
-    "Way too low!"
-  elsif your_guess.to_i > NUMBER
-    "Too high!"
-  elsif your_guess.to_i < NUMBER
-    "Too low!"
+def guess_limiter(your_guess)
+  if @@counter == 0
+    @@counter = 5
+    @@number = rand(101)
+    "Nope, you're out of guesses and you've lost. A new number has been generated."
   else
-    "The SECRET NUMBER is #{NUMBER}"
+    @@counter -= 1
+    resulting_guess(your_guess)
+  end
+end
+
+def resulting_guess(your_guess)
+  if your_guess.to_i == @@number
+    "You got it right with #{@@counter} guesses left! The SECRET NUMBER is #{@@number}"
+  elsif your_guess.to_i > @@number + 5
+    "Way too high! You have #{@@counter} guesses left."
+  elsif your_guess.to_i < @@number - 5
+    "Way too low! You have #{@@counter} guesses left."
+  elsif your_guess.to_i > @@number
+    "Too high! You have #{@@counter} guesses left."
+  else  your_guess.to_i < @@number
+    "Too low! You have #{@@counter} guesses left."
   end
 end
 
 get '/' do
   your_guess = params["guess"]
-  message = resulting_message(your_guess)
-  erb :index, :locals => {:number => NUMBER, :message => message}
+  message = guess_limiter(your_guess)
+  erb :index, :locals => {:number => @@number, :message => message}
 end
 
